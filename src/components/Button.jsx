1a1d71a7adfefc90bae93bf5c6d1e7070f439b46
Vikash Kumar <vikash.kumar@kongsbergdigital.com>
2025-07-02 00:40:35 +0530

    Update personal information and images in the portfolio

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -13,7 +13,7 @@ const Button = ({ text, className, id }) => {
         const target = document.getElementById("counter"); // Find the section with ID "counter"
 
         // Only scroll if we found the section and an ID is passed in
-        // taht prevents the contact button from scrolling to the top
+        // that prevents the contact button from scrolling to the top
         if (target && id) {
           const offset = window.innerHeight * 0.15; // Leave a bit of space at the top
 
